f573f2217d874feaa2e3d03f725ba27f176f16ea
StephonAutery <stephon@stephonautery.com>
2020-04-10 07:09:43 -0700

    still weird.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 2020-readme-generator
+# Stephon Autery is awesome!     
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="142" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="a"><rect width="142" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#a)"><path fill="#555" d="M0 0h55v20H0z"/><path fill="#007ec6" d="M55 0h87v20H55z"/><path fill="url(#b)" d="M0 0h142v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110"> <text x="285" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="450">stephon</text><text x="285" y="140" transform="scale(.1)" textLength="450">stephon</text><text x="975" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="770">he&apos;s awesome</text><text x="975" y="140" transform="scale(.1)" textLength="770">he&apos;s awesome</text></g> </svg>
+## description: This is an awesome project.
+this is some text. let's see how this looks on the page. maybe it'll look cool. maybe it won't. we'll see.
+## contents: This a table of contents.
+## installation documentation: Don't forget the dependencies.
--- index.js
+++ index.js
@@ -13,16 +13,16 @@ inquirer
             message: "please describe this project?",
             name: "description"
         },
-        // {
-        //     type: "input",
-        //     message: "please enter a table of contents?",
-        //     name: "contents"
-        // },
-        // {
-        //     type: "input",
-        //     message: "please explain the installation process.",
-        //     name: "installDoc"
-        // },
+        {
+            type: "input",
+            message: "please enter a table of contents?",
+            name: "contents"
+        },
+        {
+            type: "input",
+            message: "please explain the installation process.",
+            name: "installDoc"
+        },
         // {
         //     type: "input",
         //     message: "how will the app be used?",
@@ -58,17 +58,18 @@ inquirer
 
         let stephon = "stephon"
 
-       let writePage =  `# ${response.name}.
-        
-        
+       let writePage =  `# ${response.name}     
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="142" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="a"><rect width="142" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#a)"><path fill="#555" d="M0 0h55v20H0z"/><path fill="#007ec6" d="M55 0h87v20H55z"/><path fill="url(#b)" d="M0 0h142v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110"> <text x="285" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="450">stephon</text><text x="285" y="140" transform="scale(.1)" textLength="450">stephon</text><text x="975" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="770">he&apos;s awesome</text><text x="975" y="140" transform="scale(.1)" textLength="770">he&apos;s awesome</text></g> </svg>
-## ${response.description}`;
+## description: ${response.description}
+this is some text. let's see how this looks on the page. maybe it'll look cool. maybe it won't. we'll see.
+## contents: ${response.contents}
+## installation documentation: ${response.installDoc}`;
 
 
 
 
         responseJson = JSON.stringify(writePage);
-        fs.writeFile('readme-sla.md', writePage, 'utf8', (err) => {
+        fs.writeFile('README.md', writePage, 'utf8', (err) => {
             if (err) throw err;
         });
     })
