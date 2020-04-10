1c7bbe96ed5f919e52527c162221be147fc2af7e
StephonAutery <stephon@stephonautery.com>
2020-04-10 15:22:26 -0700

    functional

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-# this is it     
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="142" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="a"><rect width="142" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#a)"><path fill="#555" d="M0 0h55v20H0z"/><path fill="#007ec6" d="M55 0h87v20H55z"/><path fill="url(#b)" d="M0 0h142v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110"> <text x="285" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="450">stephon</text><text x="285" y="140" transform="scale(.1)" textLength="450">stephon</text><text x="975" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="770">he&apos;s awesome</text><text x="975" y="140" transform="scale(.1)" textLength="770">he&apos;s awesome</text></g> </svg>
-## description: 
- thi is
-this is some text. let's see how this looks on the page. maybe it'll look cool. maybe it won't. we'll see.
-## contents: 
+# This is Awesome! 
+ ![i'm a link](https://img.shields.io/badge/license-MIT-blue.svg) 
+ ## description: 
+ this it this is some text. let's see how this looks on the page. maybe it'll look cool. maybe it won't. we'll see. 
+ ## contents: 
   
  - this is the place for your table of contents 
  - you can make a list here 
- - this is how it works.
-## installation documentation: 
- thi is 
-  ![i'm a link](https://img.shields.io/badge/license-MIT-blue.svg)  
-   
- #### this is the default message for the editor. 
+ - this is how it works. 
+ ## installation documentation: 
+ this is installed 
+ ## how will the app be used? : 
+  
+ this is the default message for the editor. 
  * what will this do. 
  * will it work twice? 
  * will it work again?
--- index.js
+++ index.js
@@ -30,7 +30,7 @@ inquirer
         {
             type: "editor",
             message: "how will the app be used?",
-            default: " \n #### this is the default message for the editor. \n * what will this do. \n * will it work twice? \n * will it work again?",
+            default: " \n this is the default message for the editor. \n * what will this do. \n * will it work twice? \n * will it work again?",
             name: "usage"
         },
         {
@@ -61,16 +61,7 @@ inquirer
         // console.log(response.description);
         // console.log(response.contents);
 
-        let stephon = "stephon"
-
-       let writePage =  `# ${response.name}     
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="142" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><clipPath id="a"><rect width="142" height="20" rx="3" fill="#fff"/></clipPath><g clip-path="url(#a)"><path fill="#555" d="M0 0h55v20H0z"/><path fill="#007ec6" d="M55 0h87v20H55z"/><path fill="url(#b)" d="M0 0h142v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="110"> <text x="285" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="450">stephon</text><text x="285" y="140" transform="scale(.1)" textLength="450">stephon</text><text x="975" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="770">he&apos;s awesome</text><text x="975" y="140" transform="scale(.1)" textLength="770">he&apos;s awesome</text></g> </svg>
-## description: \n ${response.description}
-this is some text. let's see how this looks on the page. maybe it'll look cool. maybe it won't. we'll see.
-## contents: \n ${response.contents}
-## installation documentation: \n ${response.installDoc} 
-  ![i'm a link](https://img.shields.io/badge/license-MIT-blue.svg)  
-  ${response.usage}`;
+        let writePage = `# ${response.name} \n ![i'm a link](https://img.shields.io/badge/license-MIT-blue.svg) \n ## description: \n ${response.description} this is some text. let's see how this looks on the page. maybe it'll look cool. maybe it won't. we'll see. \n ## contents: \n ${response.contents} \n ## installation documentation: \n ${response.installDoc} \n ## how will the app be used? : \n ${response.usage}`;
 
 
 
